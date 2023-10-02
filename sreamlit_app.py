fdd3f2cbb33890a02e091d659cabfd3ead54f9b0
yogitaaa11 <145833876+yogitaaa11@users.noreply.github.com>
2023-10-02 12:15:41 +0530

    Update sreamlit_app.py
    added breakfast menu header and 3 items

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,1 +1,8 @@
 
+import sreamlit_app
+sreamlit.title('Yogita the great')
+
+sreamlit.header('Breakfast Menu')
+sreamlit.text('Omega 3 & Blueberry Oatmeal')
+sreamlit.text('Kale, Spinach & Rocket Smoothie')
+sreamlit.text('Hard-Boiled Free-Range Egg')
